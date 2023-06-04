1f7d1bf58431d8371b9558f60e053839cbd8eb11
OlgaNorina <60429064+OlgaNorina@users.noreply.github.com>
2023-06-04 16:42:32 +0300

    Update int32_to_ip.py

--- int32_to_ip.py
+++ int32_to_ip.py
@@ -1,9 +1,9 @@
-def int32_to_ip(a):
-     b = f"{a:08x}"
-     c=[b[i:i+2] for i in range(0, len(b), 2)]
-     d=[int(s, 16) for s in c]
-     e=[str(i) for i in d]
-     return '.'.join(e)
+def int32_to_ip(int32):
+     hex_str = f"{int32:08x}"
+     hex_str_list = [hex_str[i:i+2] for i in range(0, len(hex_str), 2)]
+     int_list = [int(s, 16) for s in hex_str_list]
+     dec_str_list = [str(i) for i in int_list]
+     return '.'.join(dec_str_list)
       
 
 assert int32_to_ip(2154959208) == "128.114.17.104"
