2b0a521c0624aefeca1e1bfba2090f902ae72259
OlgaNorina <60429064+OlgaNorina@users.noreply.github.com>
2023-06-04 21:00:18 +0300

    Update zeros.py

--- zeros.py
+++ zeros.py
@@ -1,18 +1,9 @@
 def zeros(n):
-  factorial = 1
-
-  for i in range(1, n+1):
-    factorial = factorial * i
-
-  count = 0
-  fact_str = str(factorial)
-  rev_fact_str = fact_str[::-1]
-  for i in rev_fact_str:
-        if(i != "0"):
-            break
-        else:
-            count += 1
-  return count
+    count=0
+    while n >= 5:
+        n //= 5
+        count += n
+    return count
 
 assert zeros(0) == 0
 assert zeros(6) == 1
